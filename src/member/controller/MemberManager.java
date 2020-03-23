42df47f2bae57702b233baa7eeab3bded15b308d
user2 <user2@DESKTOP-3UTV0OG>
2020-03-23 20:27:58 +0900

    MemberManager 클래스의 insertMember 메소드 추가

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -1,9 +1,37 @@
 package member.controller;
 
-public class MemberManager {
+import java.util.Scanner;
 
+import member.model.vo.Member;
+
+public class MemberManager {
+	
+	private static Member[] m = new Member[10];
+	private static int ctn;
+	private Scanner sc = new Scanner(System.in);
+	
+	public MemberManager() {}
+
+	
 	public void insertMember() {
-		// TODO Auto-generated method stub
+		System.out.print("아이디 입력");
+		String userId = sc.next();
+		System.out.print("패스워드 입력");
+		String userPwd = sc.next();
+		System.out.print("이름 입력");
+		String userName = sc.next();
+		System.out.print("나이 입력");
+		int age = sc.nextInt();
+		System.out.print("성별 입력");
+		char gender = sc.next().charAt(0);
+		System.out.print("이메일 입력");
+		String email = sc.next();
+		int i = 0;
+		Member newMember = new Member(userId, userPwd, userName, age, gender, email);
+		m[i] = newMember;
+		i++;
+		System.out.println("입력이 완료되었습니다. 메인메뉴로 돌아갑니다.");
+		return;
 		
 	}
 

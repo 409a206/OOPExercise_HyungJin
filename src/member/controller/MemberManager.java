90c563151fb4eb9bdcd8e9a0f30ea81549a050e7
user2 <user2@DESKTOP-3UTV0OG>
2020-03-23 20:08:31 +0900

    MemberManager 클래스에 mainMenu 메소드 추가

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -2,4 +2,14 @@ package member.controller;
 
 public class MemberManager {
 
+	public void insertMember() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void printAllMember() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
